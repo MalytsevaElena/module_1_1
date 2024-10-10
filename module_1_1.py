9bf1771e47e2e7550a0a62b95aa1fb41b04ff703
MalytsevaElena <malytseva.elena@gmail.com>
2024-10-10 04:04:38 +0300

    Практическое задание по теме Переменные

---
+++ module_1_1.py
@@ -0,0 +1,4 @@
+number_of_homework = 12 # количество выполненных домашних заданий
+time_spent = 1.5 # затрачено времени
+name_of_course = 'Python' # название курса
+print("Курс:",name_of_course,", всего задач:",number_of_homework,", затрачено часов:", time_spent, ", среднее время выполнения", time_spent / number_of_homework, "часа." )
